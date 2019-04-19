3c7eadf4b2fce684f5cf716b78db5a168d803890
carmenpmoreno <carmenp.moreno@gmail.com>
2019-04-19 18:21:18 +0200

    initial function to picture de Array

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,44 @@
 'use strict';
 
 // FUNCION DE APOYO:contador de números aleatorios
-const randomNumber = getRandomNumber(10);
 // console.log(randomNumber);
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
   }
+const randomNumber = getRandomNumber(10);
+console.log(randomNumber);
+
+//Nueva tarea: Array que queremos pintar en la consola
+const myWordList =  [
+    {
+      text: 'Pencil',
+      total: 6
+    },
+    {
+      text: 'Thermo',
+      total: 2
+    },
+    {
+      text: 'TV',
+      total: 8
+    },
+    {
+      text: 'Phone',
+      total: 4
+    }
+  ];
+// escribimos una nueva función, la cual:
+function writeMyArray(word, randomNumber) {
+    // recorra el array "myWordList"
+    for (let i=0; i<myWordList.length; i++) {
+        
+        console.log(word);
+    }
+    // use la función "writeThis" para pintar el Array
+
+}
+
+// FUNCION DE APOYO "writeThis"
 
 function writeThis(word, lenght) {
     for (let i=0; i<lenght; i++) {
@@ -13,7 +46,8 @@ function writeThis(word, lenght) {
     }
 }
 
-writeThis ('Patata', randomNumber);
-writeThis ('Aguacate', randomNumber);
-writeThis('Pizza', randomNumber);
+
+
+
+writeThis(myWordList, randomNumber);
 
