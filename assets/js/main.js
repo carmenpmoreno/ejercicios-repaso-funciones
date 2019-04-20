79a612da20317380eb2a64b3e47a75bca511f541
carmenpmoreno <carmenp.moreno@gmail.com>
2019-04-20 13:39:09 +0200

    try to picture array objects on console

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,51 +3,46 @@
 // FUNCION DE APOYO:contador de números aleatorios
 // console.log(randomNumber);
 function getRandomNumber(max) {
-    return Math.ceil(Math.random() * max);
-  }
+  return Math.ceil(Math.random() * max);
+}
 const randomNumber = getRandomNumber(10);
 console.log(randomNumber);
 
 //Nueva tarea: Array que queremos pintar en la consola
-const myWordList =  [
-    {
-      text: 'Pencil',
-      total: 6
-    },
-    {
-      text: 'Thermo',
-      total: 2
-    },
-    {
-      text: 'TV',
-      total: 8
-    },
-    {
-      text: 'Phone',
-      total: 4
-    }
-  ];
+const myWordList = [
+  {
+    text: 'Pencil',
+    total: 6
+  },
+  {
+    text: 'Thermo',
+    total: 2
+  },
+  {
+    text: 'TV',
+    total: 8
+  },
+  {
+    text: 'Phone',
+    total: 4
+  }
+];
 // escribimos una nueva función, la cual:
-function writeMyArray(word, randomNumber) {
-    // recorra el array "myWordList"
-    for (let i=0; i<myWordList.length; i++) {
-        
-        console.log(word);
-    }
-    // use la función "writeThis" para pintar el Array
-
+function writeMyArray(word, lenght) {
+  // recorra el array "myWordList"
+  for (let i = 0; i < randomNumber; i++) {
+    console.log(myWordList[i]);
+  }
 }
+const arrayFunction = writeMyArray(myWordList, randomNumber);
 
-// FUNCION DE APOYO "writeThis"
+// // FUNCION DE APOYO "writeThis"
 
-function writeThis(word, lenght) {
-    for (let i=0; i<lenght; i++) {
-        console.log(word);
-    }
-}
+// function writeThis(word, lenght) {
+//   for (let i = 0; i < lenght; i++) {
+//     console.log(word);
+//   }
+// }
 
-
-
-
-writeThis(myWordList, randomNumber);
+// writeThis(arrayFunction, randomNumber);
 
